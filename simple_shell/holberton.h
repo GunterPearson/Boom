1d445e2973abd43301263752c4654063454592f5
GunterPearson <1715@holbertonschool.com>
2020-04-03 18:09:31 +0000

    added header file

---
+++ simple_shell/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_
+#define HOLBERTON_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif
